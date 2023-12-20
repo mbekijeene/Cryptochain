9cb19570e6efbd055853ac3e3a942382e9ac62a2
mojjoiv <daltonongeche@gmail.com>
2023-12-20 23:32:06 +0300

    cryptochain

---
+++ block.js
@@ -0,0 +1,21 @@
+class Block{
+    constructor({timestamp, lastHash, hash, data}) {
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.block = block;
+        this.data = data;
+    }
+}
+
+const block1 = new Block ({
+    timestamp:'01/01/24', 
+    lastHash: 'foo- lastHash', 
+    hash: 'foo- hash', 
+    data: 'foo- data'
+
+
+
+
+})
+
+console.log('block1','block1');
