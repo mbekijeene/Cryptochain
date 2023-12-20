5a071baa44a880d67f6b206dde468809d339707b
mbekijeene <dmbeki14@gmail.com>
2023-12-21 00:25:27 +0300

    added tests driven development to the project

--- block.js
+++ block.js
@@ -1,21 +1,10 @@
 class Block{
-    constructor({timestamp, lastHash, hash, data}) {
-        this.timestamp = timestamp;
-        this.lastHash = lastHash;
-        this.block = block;
-        this.data = data;
-    }
+constructor({ timestamp, lastHash, hash, data}) {
+this.timestamp = timestamp;
+this.lastHash = lastHash;
+this.hash = hash;
+this.data = data;
+}
 }
 
-const block1 = new Block ({
-    timestamp:'01/01/24', 
-    lastHash: 'foo- lastHash', 
-    hash: 'foo- hash', 
-    data: 'foo- data'
-
-
-
-
-})
-
-console.log('block1','block1');
+module.exports = Block;
